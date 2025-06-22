32a39a942af576092722f58fa213f1522bc9e354
Noa Mari <noamari@Edis-MacBook-Pro.local>
2025-06-22 18:25:44 +0300

    add ICollectionOperations and CollectionOperations templet

---
+++ collection/CollectionOperations.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class CollectionOperations : ICollectionOperations
+{
+    public void ListOperations(List<string> list)
+    {
+       
+    }
+
+    private void loopPrint(List<string> list)
+    {
+       
+    }
+
+    public void DictionaryOperations(Dictionary<string, int> dictionary)
+    {
+      
+
+    }
+    public void StackOperations(Stack<DateTime> stack)
+    {
+       
+    }
+}
---
+++ collection/ICollectionOperations.cs
@@ -0,0 +1,9 @@
+public interface ICollectionOperations
+{
+    void ListOperations(List<string> list);
+
+    void DictionaryOperations(Dictionary<string, int> dictionary);
+
+    void StackOperations(Stack<DateTime> stack);
+
+}
